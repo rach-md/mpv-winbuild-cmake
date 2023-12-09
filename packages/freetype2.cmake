3ef0bb27aaee54ce779702163b7a249e847d4e1e
rach-md <rach-md@users.noreply.github.com>
2023-12-09 22:06:37 +0800

    freetype2: remove brotli

--- packages/freetype2.cmake
+++ packages/freetype2.cmake
@@ -2,7 +2,6 @@ ExternalProject_Add(freetype2
     DEPENDS
         libpng
         zlib
-        brotli
     GIT_REPOSITORY https://github.com/freetype/freetype.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
@@ -16,7 +15,6 @@ ExternalProject_Add(freetype2
         --default-library=static
         -Dharfbuzz=disabled
         -Dtests=disabled
-        -Dbrotli=enabled
         -Dzlib=enabled
         -Dpng=enabled
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
