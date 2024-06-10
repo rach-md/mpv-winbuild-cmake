6caa41c05ad42eb308851635e283d8761c305e62
rach-md <rach-md@users.noreply.github.com>
2024-06-10 20:39:42 +0800

    Revert "libass: switch to meson"
    This reverts commit 0a725f5f920f2355c5b5c720c7e86f42b429492a.

--- packages/libass.cmake
+++ packages/libass.cmake
@@ -9,21 +9,15 @@ ExternalProject_Add(libass
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
+    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
-        --libdir=${MINGW_INSTALL_PREFIX}/lib
-        --cross-file=${MESON_CROSS}
-        --buildtype=release
-        --default-library=static
-        -Dfontconfig=disabled
-        -Ddirectwrite=enabled
-        -Dasm=enabled
-        -Dlibunibreak=enabled
-    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
-    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+        --disable-shared
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libass)
-force_meson_configure(libass)
 cleanup(libass install)
